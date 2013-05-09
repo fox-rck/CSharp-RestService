2f66099203622cecf89a9a52afb1228a98012ea8
fox-rck <fox.rck@gmail.com>
2013-05-09 00:42:15 -0500

    Init

---
+++ RestService/IRestServiceImpl.cs
@@ -0,0 +1,70 @@
+﻿using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.IO;
+
+namespace RestService
+{
+    
+    [ServiceContract]
+    public interface IRestServiceImpl
+    {
+        [OperationContract]
+        [WebInvoke(Method = "GET",
+            ResponseFormat = WebMessageFormat.Xml,
+            BodyStyle = WebMessageBodyStyle.Wrapped,
+            UriTemplate = "xml/{id}")]
+        string GET_xmlData(string id);
+
+        [OperationContract]
+        [WebInvoke(Method = "GET",
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Wrapped,
+            UriTemplate = "json/{id}")]
+        string GET_jsonData(string id);
+
+        [OperationContract]
+        [WebInvoke(Method = "PUT",
+            ResponseFormat = WebMessageFormat.Xml,
+            RequestFormat = WebMessageFormat.Xml,
+            BodyStyle = WebMessageBodyStyle.Wrapped,
+            UriTemplate = "xml/{id}")]
+        string UPDATE_xmlData(string id);
+
+        [OperationContract]
+        [WebInvoke(Method = "PUT",
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Wrapped,
+            UriTemplate = "json/{id}")]
+        string UPDATE_jsonData(string id);
+
+        [OperationContract]
+        [WebInvoke(Method = "DELETE",
+            ResponseFormat = WebMessageFormat.Xml,
+            RequestFormat = WebMessageFormat.Xml,
+            BodyStyle = WebMessageBodyStyle.Wrapped,
+            UriTemplate = "xml/{id}")]
+        string DELETE_xmlData(string id);
+
+        [OperationContract]
+        [WebInvoke(Method = "DELETE",
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Wrapped,
+            UriTemplate = "json/{id}")]
+        string DELETE_jsonData(string id);
+
+        [OperationContract]
+        [WebInvoke(Method = "POST",
+            ResponseFormat = WebMessageFormat.Xml,
+            RequestFormat=WebMessageFormat.Xml,
+            BodyStyle = WebMessageBodyStyle.Wrapped,
+            UriTemplate = "xml/{id}")]
+        string CREATE_xmlData(string streamdata, string id);
+
+        [OperationContract]
+        [WebInvoke(Method = "POST",
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Wrapped,
+            UriTemplate = "json/{id}")]
+        string CREATE_jsonData(string Name,string Age, string id);
+    }
+}
---
+++ RestService/RestServiceImpl.svc.cs
@@ -0,0 +1,117 @@
+﻿using System.IO;
+namespace RestService
+{
+    public class RestServiceImpl : IRestServiceImpl
+    {
+        #region IRestServiceImpl Members
+
+        private bool isvalidated(string x) {
+            //TO DO: ADD Authentication Logic
+            var y = true;
+            return y;
+        }
+        public string GET_xmlData(string id)
+        {
+            if(isvalidated(id)){
+                try{
+            return "You requested product " + id;
+                }catch{
+                    return "Oopps something went wrong";
+                }
+            }else{
+            return "User Not authenticated";
+            }
+        }
+
+        public string GET_jsonData(string id)
+        {
+            if(isvalidated(id)){
+                try{
+            return "You requested product " + id;
+                }catch{
+                    return "Oopps something went wrong";
+                }
+            }else{
+            return "User Not authenticated";
+            }
+        }
+
+        public string DELETE_xmlData(string id)
+        {
+            if(isvalidated(id)){
+                try{
+            return "You requested to DELETE " + id;
+                }catch{
+                    return "Oopps something went wrong";
+                }
+            }else{
+            return "User Not authenticated";
+            }
+
+        }
+        public string DELETE_jsonData(string id)
+        {
+                 if(isvalidated(id)){
+                try{
+            return "You requested to DELETE " + id;
+                }catch{
+                    return "Oopps something went wrong";
+                }
+            }else{
+            return "User Not authenticated";
+            }
+        }
+        public string CREATE_xmlData(string streamdata, string id)
+        {
+                if(isvalidated(id)){
+                try{
+
+                    return "Received: " + streamdata;
+                }catch{
+                    return "Oopps something went wrong";
+                }
+            }else{
+            return "User Not authenticated";
+            }
+        }
+        public string CREATE_jsonData(string streamdata,string age, string id)
+        {
+            if(isvalidated(id)){
+                try{
+                    return "Received: " + streamdata +" is "+ age +" years old";
+                  
+                }catch{
+                    return "Oopps something went wrong";
+                }
+            }else{
+            return "User Not authenticated";
+            }
+        }
+        public string UPDATE_xmlData(string id)
+        {
+             if(isvalidated(id)){
+                try{
+            return "You requested to UPDATE " + id;
+                }catch{
+                    return "Oopps something went wrong";
+                }
+            }else{
+            return "User Not authenticated";
+            }
+        }
+        public string UPDATE_jsonData(string id)
+        {
+            if(isvalidated(id)){
+                try{
+            return "You requested to UPDATE " + id;
+                }catch{
+                    return "Oopps something went wrong";
+                }
+            }else{
+            return "User Not authenticated";
+            }
+        }
+
+        #endregion
+    }
+}
